7f0bf136d1495d3bb787d76e34fe987ac5896df7
kento tajiri <k.denjiry@gmail.com>
2020-08-01 18:31:59 +0900

    implement tokenizer wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,22 @@ use lindera::tokenizer::Tokenizer;
 
 #[cfg(test)]
 mod tests {
+    use crate::*;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        if let Ok(parsed) = parse("日常的な推論でも、数学的に証明できるはずである。")
+        {
+            assert!(parsed.len() > 0);
+        }
     }
+}
+
+pub fn parse(ja: &str) -> std::io::Result<Vec<String>> {
+    let mut tokenizer = Tokenizer::new("normal", "");
+    let tokens = tokenizer.tokenize(&ja);
+    let mut ret: Vec<String> = Vec::new();
+    for token in tokens {
+        ret.push(token.text.to_string());
+    }
+    Ok(ret)
 }
