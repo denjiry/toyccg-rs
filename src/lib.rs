2a9cf1900e2c2cc7d3795c6822062fbce81ae3c2
kento tajiri <k.denjiry@gmail.com>
2020-08-08 18:17:09 +0900

    add build_chart

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,24 @@ mod tests {
             assert!(tokenized.len() > 0);
         }
     }
+    #[test]
+    fn test_buildChart() {
+        let test_str = "日常的な推論でも、数学的に証明できるはずである。";
+        let Ok(tokenized) = tokenize(test_str);
+        let chart = build_chart(tokenized);
+        assert_eq!(chart.len(), tokenized.len());
+    }
+}
+
+pub fn build_chart(tokens: Vec<String>) -> Vec<Vec<String>> {
+    let chart_size = tokens.len();
+    let mut chart = vec![vec![String::new(); chart_size]; chart_size];
+    for n in 0..chart_size {
+        for m in n..chart_size {
+            chart[n][m] = "a".to_string();
+        }
+    }
+    chart
 }
 
 pub fn tokenize(ja: &str) -> std::io::Result<Vec<String>> {
